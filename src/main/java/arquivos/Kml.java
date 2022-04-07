d6c691efe46d9b77b0160992875ce85f8b54c89d
mariacarolinass <m.carolina.soares1@gmail.com>
2022-04-07 08:27:42 -0300

    add coord em arquivo kml

---
+++ src/main/java/arquivos/Kml.java
@@ -0,0 +1,46 @@
+package arquivos;
+
+import utils.CoordUtils;
+import coordenada.Coord;
+import java.util.ArrayList;
+import java.util.Map;
+
+public class Kml {
+    
+    public static void exportKml(String nome, Map<String, ArrayList<Coord>> dadosCoord ) {
+        
+        ArrayList<Coord> listaCoord = dadosCoord.get(nome);
+        
+        // construindo documento kml
+        StringBuilder builder = new StringBuilder();
+
+        builder.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
+        builder.append("<kml xmlns=\"http://www.opengis.net/kml/2.2\">\n");
+        builder.append("<Document>\n");
+        builder.append("<name>Coordenadas</name>\n");
+        
+        builder.append("<Placemark>\n");
+        builder.append("<LineString>\n");
+        builder.append("<coordinates>\n");
+        
+        // passando as coordenadas latitude e longitude
+        for (Coord coordenadas: listaCoord) {
+        
+            builder.append(CoordUtils.convertLonDMStoDD(coordenadas.getLon()) +
+                ","+CoordUtils.convertLatDMStoDD(coordenadas.getLat())+",0");
+            builder.append("\n");
+    
+        }
+    
+        builder.append("</coordinates>\n");
+        builder.append("</LineString>\n");
+        builder.append("</Placemark>\n");
+        builder.append("</Document>\n");
+        builder.append("</kml>\n");
+        
+        System.out.println("Arquivo kml construido!");
+        //System.out.println(builder.toString());
+        
+    }
+    
+}
